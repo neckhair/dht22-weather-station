412d6c4ae5cada15b74a18800ba234d2b090d006
Philippe Hässig <phil@nine.ch>
2016-12-24 13:21:23 +0100

    Switch to change mode
    Connect a push button to pin A0 an read its value. Whenever the value is 1 we switch the mode to either temperature or
    humidity.

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,36 @@
 dht DHT;
 
 #define DHT22_PIN 6
+#define MODE_SWITCH_PIN A0
 
-void setup() {
-  for ( int i = 0; i < numberOfDigits; i++ )  pinMode(digits[i], OUTPUT);
-  for ( int i = 0; i < numberOfSegments; i++) pinMode(segments[i], OUTPUT);
+#define MODE_TEMP     0b01
+#define MODE_HUMIDITY 0b10
 
-  turnAllSegmentsOff();
+byte currentMode = MODE_TEMP;
+unsigned long modeLastSwitched = 0;
+
+float currentValue() {
+  switch(currentMode) {
+    case MODE_TEMP:
+      return DHT.temperature;
+    case MODE_HUMIDITY:
+      return DHT.humidity;
+    default:
+      return 0.0;
+  }
 }
 
-void loop() {
+void readAndDisplayValue() {
   int chk = DHT.read22(DHT22_PIN);
   switch (chk)
   {
     case DHTLIB_OK:
-
       for( int timer = 0; timer < 200; timer++ ) {
         for ( byte digit = 0; digit < numberOfDigits; digit++) {
-          byte figure = extractDigit(DHT.temperature, digit);
+          byte figure = extractDigit(currentValue(), digit);
           displayFigure(digits[digit], figure);
         }
       }
-
       break;
     case DHTLIB_ERROR_CHECKSUM:
       break;
@@ -34,4 +43,28 @@ void loop() {
     default:
       break;
   }
+}
+
+void setMode() {
+  unsigned int modeSwitch = digitalRead(MODE_SWITCH_PIN);
+
+  if( modeSwitch == LOW) return;
+  if( millis() - modeLastSwitched < 200 ) return; // Switch only every x millis
+
+  currentMode = currentMode ^ 0b11;
+  modeLastSwitched = millis();
+}
+
+void setup() {
+  for ( int i = 0; i < numberOfDigits; i++ )  pinMode(digits[i], OUTPUT);
+  for ( int i = 0; i < numberOfSegments; i++) pinMode(segments[i], OUTPUT);
+
+  pinMode(MODE_SWITCH_PIN, INPUT);
+
+  turnAllSegmentsOff();
+}
+
+void loop() {
+  setMode();
+  readAndDisplayValue();
 }
