7813b7df9d92b98297e728e81aa39471d13e0940
Philippe Hässig <phil@nine.ch>
2017-01-01 20:07:42 +0100

    Use a 74HC595 shift register to spare some pins

--- src/display.h
+++ src/display.h
@@ -3,45 +3,40 @@
 #define FRAMEPERIOD 2000
 #define BRIGHTNESS_DELAY 200
 
-#define DIGIT_1 7
-#define DIGIT_2 4
-#define DIGIT_3 3
+#define DIGIT_1 12
+#define DIGIT_2 11
+#define DIGIT_3 10
 #define DIGIT_4 9
-#define SEGMENT_G 8
-#define SEGMENT_F 5
-#define SEGMENT_A 6
-#define SEGMENT_B 2
-#define SEGMENT_E 12
-#define SEGMENT_D 11
-#define SEGMENT_C 10
-#define DECIMAL_POINT A0
+
+#define LATCH_PIN 4
+#define CLOCK_PIN 5
+#define DATA_PIN 3
 
 const boolean DigitOn = LOW;
 const boolean DigitOff = HIGH;
 const boolean SegmentOn = HIGH;
 const boolean SegmentOff = LOW;
 
-const int segments[] = { SEGMENT_A, SEGMENT_B, SEGMENT_C, SEGMENT_D, SEGMENT_E, SEGMENT_F, SEGMENT_G };
 const int digits[] = { DIGIT_4, DIGIT_3, DIGIT_2, DIGIT_1 };
 
 const int digitMasks[] = {
-// ABCDEFG
-  0b1111110, // 0
-  0b0110000, // 1
-  0b1101101, // 2
-  0b1111001, // 3
-  0b0110011, // 4
-  0b1011011, // 5
-  0b1011111, // 6
-  0b1110000, // 7
-  0b1111111, // 8
-  0b1111011, // 9
+  // ABCDEFG
+  0b01111110, // 0
+  0b00110000, // 1
+  0b01101101, // 2
+  0b01111001, // 3
+  0b00110011, // 4
+  0b01011011, // 5
+  0b01011111, // 6
+  0b01110000, // 7
+  0b01111111, // 8
+  0b01111011, // 9
 };
 
 const char letterMasks[] = {
-  0b1001110, // C for Temperature in Celsius
-  0b0110111, // H for Humidity in Percent
-  0b1001111, // E for errors
+  0b01001110, // C for Temperature in Celsius
+  0b00110111, // H for Humidity in Percent
+  0b01001111, // E for errors
 };
 
 class Display {
